8b23b5bc48c6d54b8aa4d861ac67b629cfd03514
Rafael Reis <rafael.dnreis@gmail.com>
2026-05-28 15:56:28 -0300

    test: align catalog spec with material forms

--- src/app/components/catalog/catalog.component.spec.ts
+++ src/app/components/catalog/catalog.component.spec.ts
@@ -2,6 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { CatalogComponent } from './catalog.component';
 
 describe('CatalogComponent', () => {
@@ -10,7 +17,17 @@ describe('CatalogComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule, HttpClientTestingModule],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        NoopAnimationsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+      ],
       declarations: [CatalogComponent],
       schemas: [NO_ERRORS_SCHEMA],
     });
